7add512ca8fffd65717299ed1da3173305af53a1
Mike <dev.mhowells@gmail.com>
2022-05-16 13:48:54 +0100

    completed main functionality of comments section and moved to own component

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Flashcards from "./Flashcards";
+import Comments from "./Comments";
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
@@ -66,40 +67,60 @@ export default function Article() {
     />
   ));
 
-  // -------------------------- USER STORY -------------------------
+  // // -------------------------- USER STORY -------------------------
 
-  const [userStory, setUserStory] = React.useState("");
-  const [checklist, setChecklist] = React.useState([]);
+  // const [userStory, setUserStory] = React.useState("");
+  // const [checklist, setChecklist] = React.useState([]);
+  // const [usedWords, setUsedWords] = React.useState([]);
 
-  // set state for checklist inside useEffect because it was creating an infinite loop
-  // can't work out why right now, but this fixes it...
-  React.useEffect(() => {
-    let allFlashyTitles = [];
-    // loops over array of flashcards, creates new array for each flashcard, takes title
-    for (let i in flashcards) {
-      let flashTitles = flashcards[i].map((flashcard) => flashcard.title);
-      allFlashyTitles = [...allFlashyTitles, ...flashTitles];
-      setChecklist(allFlashyTitles);
-    }
-  }, []);
+  // // set state for checklist inside useEffect because it was creating an infinite loop
+  // // can't work out why right now, but this fixes it...
+  // // also needs to run more than once or else list does not load on initial page-load
+  // // I think because flashcards would be an empty array at that time
+  // // therefore runs again on flashcard state change
+  // React.useEffect(() => {
+  //   let allFlashyTitles = [];
+  //   // loops over array of flashcards, creates new array for each flashcard, takes title
+  //   for (let i in flashcards) {
+  //     let flashTitles = flashcards[i].map((flashcard) => flashcard.title);
+  //     allFlashyTitles = [...allFlashyTitles, ...flashTitles];
+  //     setChecklist(allFlashyTitles);
+  //   }
+  // }, [flashcards]);
 
-  console.log("setcheck", checklist);
+  // // reads input on keystroke, checks if checklist word is written, if so adds to usedWords
+  // // also checks usedWords, if item in usedWords is no longer in input, removes
+  // // from usedWords
+  // function readStory(e) {
+  //   setUserStory(e.target.value);
 
-  function readStory(e) {
-    setUserStory(e.target.value);
+  //   for (let i in checklist) {
+  //     if (
+  //       userStory.includes(checklist[i]) &&
+  //       !usedWords.includes(checklist[i])
+  //     ) {
+  //       setUsedWords((prevUsedWords) => [...prevUsedWords, checklist[i]]);
+  //     } else if (
+  //       !userStory.includes(checklist[i]) &&
+  //       usedWords.includes(checklist[i])
+  //     ) {
+  //       setUsedWords((prevUsedWords) =>
+  //         prevUsedWords.filter((word) => word !== checklist[i])
+  //       );
+  //     }
+  //   }
+  // }
+  // console.log(usedWords);
 
-    for (let i in checklist) {
-      if (userStory.includes(checklist[i])) {
-        console.log(checklist[i]);
-      }
-    }
-  }
+  // const checklistDisplay = checklist.map((title) => (
+  //   <p
+  //     className={`checklist-items ${usedWords.includes(title) && "used-word"}`}
+  //   >
+  //     {title}
+  //   </p>
+  // ));
 
-  const checklistDisplay = checklist.map((title) => (
-    <p className="checklist-items">{title}</p>
-  ));
-
-  console.log(userStory);
+  // console.log(userStory);
 
   return (
     <div className="article-container">
@@ -124,10 +145,11 @@ export default function Article() {
           deleteCard={deleteCard}
         />
       )}
-      <div className="comment-section">
+      {/* <div className="comment-section">
         <textarea className="textarea" onChange={readStory}></textarea>
         <div className="checklist-container">{checklistDisplay}</div>
-      </div>
+      </div> */}
+      <Comments flashcards={flashcards} />
     </div>
   );
 }
---
+++ src/Comments.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default function Comments(props) {
+  const [userStory, setUserStory] = React.useState("");
+  const [checklist, setChecklist] = React.useState([]);
+  const [usedWords, setUsedWords] = React.useState([]);
+
+  // set state for checklist inside useEffect because it was creating an infinite loop
+  // can't work out why right now, but this fixes it...
+  // also needs to run more than once or else list does not load on initial page-load
+  // I think because flashcards would be an empty array at that time
+  // therefore runs again on flashcard state change
+  React.useEffect(() => {
+    let allFlashyTitles = [];
+    // loops over array of flashcards, creates new array for each flashcard, takes title
+    for (let i in props.flashcards) {
+      let flashTitles = props.flashcards[i].map((flashcard) => flashcard.title);
+      allFlashyTitles = [...allFlashyTitles, ...flashTitles];
+      setChecklist(allFlashyTitles);
+    }
+  }, [props.flashcards]);
+
+  // reads input on keystroke, checks if checklist word is written, if so adds to usedWords
+  // also checks usedWords, if item in usedWords is no longer in input, removes
+  // from usedWords
+  function readStory(e) {
+    setUserStory(e.target.value);
+
+    for (let i in checklist) {
+      if (
+        userStory.includes(checklist[i]) &&
+        !usedWords.includes(checklist[i])
+      ) {
+        setUsedWords((prevUsedWords) => [...prevUsedWords, checklist[i]]);
+      } else if (
+        !userStory.includes(checklist[i]) &&
+        usedWords.includes(checklist[i])
+      ) {
+        setUsedWords((prevUsedWords) =>
+          prevUsedWords.filter((word) => word !== checklist[i])
+        );
+      }
+    }
+  }
+  console.log(usedWords);
+
+  const checklistDisplay = checklist.map((title) => (
+    <p
+      className={`checklist-items ${usedWords.includes(title) && "used-word"}`}
+    >
+      {title}
+    </p>
+  ));
+
+  console.log(userStory);
+
+  return (
+    <div className="comment-section">
+      <textarea className="textarea" onChange={readStory}></textarea>
+      <div className="checklist-container">{checklistDisplay}</div>
+    </div>
+  );
+}
